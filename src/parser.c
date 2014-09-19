fdfec8de31fbbdc943fa7a4d3fc2b6661339a46a
Charles Lehner <celehner1@gmail.com>
2014-09-18 22:55:05 -0400

    Add more stuff

---
+++ src/parser.c
@@ -0,0 +1,1 @@
+#include <string.h>#include <Memory.h>#include "document.h"#include "parser.h"
---
+++ src/parser.h
@@ -0,0 +1,1 @@
+#ifndef _PARSER_H#define _PARSER_Htypedef enum {} ParserState;typedef struct {	ParserState state;} Parser;#endif
